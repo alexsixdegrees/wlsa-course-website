4a4a34133b8d888f8fa7680825b1fc40ef4d2651
Jialin Wu <alexsixdegrees@outlook.com>
2018-02-17 11:28:05 +0800

    creatAccount Page update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-// check the code
-if(value!="1234"){
-$(errorElement).html("The code is not correct. Should be '1234'");
-}else{
-alert('code is correct!');
-}
+
+$(document).ready(function() {
+
+// // check the code
+// if(value!="1234"){
+// $(errorElement).html("The code is not correct. Should be '1234'");
+// }else{
+// alert('code is correct!');
+// }
+
+
+$('#pincode-input1').pincodeInput({inputs:6,hidedigits:false});
+
+});
